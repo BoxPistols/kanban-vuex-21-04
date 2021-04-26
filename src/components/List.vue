b058670ffb47fd396e006e9ebf4f1578910038a4
ait <ait@ex.in>
2021-04-27 00:21:25 +0900

    Create List Components

---
+++ src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+        <div class="listheader">
+            <p class="list-title">{{ title }}</p>
+            <div class="deletelist" @click="removeList">×</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        listIndex: {
+            type: Number,
+            required: true,
+        },
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        removeList() {
+            if (confirm('削除しますか？')) {
+                this.$storestore.dispatch('removelist', { listIndex: this.listIndex })
+            }
+        },
+    },
+}
+</script>
