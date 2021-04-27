878b491736c5e93fa8e2b9177401e179cb924c65
ait <ait@ex.in>
2021-04-27 20:33:07 +0900

    Create CardAdd UI

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+        <form class="addcard" @submit.prevent="addCardToDoList">
+            <input type="text" placeholder="Add new card" class="text-input" />
+            <button type="submit" class="add-button">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardAdd',
+    props: ['listIndex'],
+    data() {
+        return {
+            body: '',
+        }
+    },
+    methods: {
+        addCardToDoList() {
+            this.$storestore.dispatch('addCardToDoList', { body: this.body, listIndex: this.listIndex })
+            this.body = ''
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
